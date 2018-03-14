62871f82edf7201ed5d9214c8f4d2bf2857875e4
Shobana G <shobana_g@photonint.com>
2018-03-14 17:10:05 +0530

    files addded/modified

--- app.js
+++ app.js
@@ -1,19 +1,29 @@
 const express = require("express");
-const path = require("path");
 const app = express();
 const bodyparser = require("body-parser");
-const expressValidator = require("express-validator");
-const MongoClient = require("mongodb").MongoClient;
 
 app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json());
-app.use(expressValidator());
-app.use("/", require("./routes/index"));
 
-MongoClient.connect("mongodb://localhost:27017", (err, client) => {
-  if (err) return console.log(err);
-  db = client.db("OnlineFruitStore");
-  server = app.listen(3000, "10.100.110.32", () => {
-    console.log("product list api  is up and running");
-  });
+app.use("/products", require("./routes/index"));
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error("Not Found");
+  err.status = 404;
+  next(err);
 });
+app.use(function(err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  res.status(err.status || 500);
+
+  res.send(err);
+});
+
+app.listen(3000, "10.100.110.32", () => {
+  console.log("product list api  is up and running");
+});
+
+module.exports = app;
---
+++ routes/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const router = express.Router();
+const mongoClient = require("mongodb").MongoClient;
+
+//1.list Product
+router.get("/", function(req, res, next) {
+  mongoClient.connect("mongodb://localhost:27017", (err, client) => {
+    if (err) {
+      console.error(err);
+      res.json(err);
+    } else {
+      db = client.db("OnlineFruitStore");
+      console.log("Connected to DB");
+      db
+        .collection("Products")
+        .find()
+        .toArray(function(err, productResults) {
+          console.log("Got Products list from db", productResults);
+          res.json(productResults);
+          client.close();
+        });
+    }
+  });
+});
+
+module.exports = router;
