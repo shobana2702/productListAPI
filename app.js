77c6c8208fa178bd8b6272b0da2b3073d99fc395
Shobana G <shobana_g@photonint.com>
2018-03-20 16:56:56 +0530

    db connectivity change

--- app.js
+++ app.js
@@ -1,11 +1,5 @@
 const app = require("express")();
 
-const mongodb = require("./routes/db");
-app.use("/", (req, res, next) => {
-  mongodb.database(err => {
-    next(err);
-  });
-});
 app.use("/products", require("./routes/index"));
 
 // catch 404 and forward to error handler
@@ -15,6 +9,7 @@ app.use((req, res, next) => {
   err.status = 404;
   next(err);
 });
+//to handle errors
 app.use((err, req, res, next) => {
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
@@ -22,30 +17,8 @@ app.use((err, req, res, next) => {
   res.send(err);
 });
 
-let server = app.listen(3000, "10.100.110.32", () => {
+app.listen(3000, "10.100.110.32", () => {
   console.log("product list api  is up and running");
 });
 
-process.on("SIGTERM", shutDown);
-process.on("SIGINT", shutDown);
-
-let connections = [];
-
-function shutDown() {
-  console.log("Received kill signal, shutting down gracefully");
-  server.close(() => {
-    console.log("Closed out remaining connections");
-    process.exit(0);
-  });
-
-  setTimeout(() => {
-    console.error(
-      "Could not close connections in time, forcefully shutting down"
-    );
-    process.exit(1);
-  }, 10000);
-
-  connections.forEach(curr => curr.end());
-  setTimeout(() => connections.forEach(curr => curr.destroy()), 5000);
-}
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -4,18 +4,24 @@ const mongodb = require("./db");
 //1.list Product
 router.get("/", (req, res, next) => {
   try {
-    mongodb
-      .getCollection()
-      .find()
-      .toArray((err, productResults) => {
-        if (err) {
-          next(err);
-        } else {
-          console.log("Products list from db", productResults);
-          res.json(productResults);
-          //mongodb.closeDb();
-        }
-      });
+    mongodb.database(err => {
+      if (err) {
+        next(err);
+      } else {
+        mongodb
+          .getCollection()
+          .find()
+          .toArray((err, productResults) => {
+            if (err) {
+              next(err);
+            } else {
+              console.log("Products list from db", productResults);
+              res.json(productResults);
+              mongodb.closeDb();
+            }
+          });
+      }
+    });
   } catch (e) {
     next(err);
   }
